5b7953e95a93fce4eafa47560ce67afcf885b6f6
Nicholas Zuber <zuber.nicholas@gmail.com>
2024-06-22 04:06:49 -0400

    filter history

--- src/components/HistoryTab.tsx
+++ src/components/HistoryTab.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { v4 as uuidv4 } from "uuid"
 import {
   emojiOfCategory,
   formatCurrency,
@@ -14,10 +15,41 @@ import {
   getKeyForDay,
   getKeyForMonth,
 } from "../types/core"
+import { Dropdown, Item } from "./Dropdown"
+
+function toItem(category: Category | null): Item<Category | null> {
+  if (!category) {
+    return {
+      id: uuidv4(),
+      title: "Everything",
+      value: null,
+    }
+  }
+
+  return {
+    id: uuidv4(),
+    title: stringOfCategory(category),
+    value: category,
+  }
+}
+
+const CATEGORY_ITEMS = [
+  toItem(null),
+  toItem(Category.Groceries),
+  toItem(Category.Transport),
+  toItem(Category.Clothes),
+  toItem(Category.Restaurants),
+  toItem(Category.Fun),
+  toItem(Category.Other),
+]
 
 export function HistoryTab() {
   const { categories, removeEntryFromCategory } = useCategories()
   const [openedId, setOpenedId] = useState<string | undefined>()
+
+  const [selectedCategory, setSelectedCategory] = useState<
+    Item<Category | null>
+  >(CATEGORY_ITEMS[0])
 
   const max = sumOfRunningMonthCategoriesMax(categories)
 
@@ -58,6 +90,12 @@ export function HistoryTab() {
     <div className="tab-container" onClick={() => setOpenedId(undefined)}>
       <div className="tab-header-container">{"History"}</div>
 
+      <Dropdown
+        selectedItem={selectedCategory}
+        items={CATEGORY_ITEMS}
+        onChange={(item: Item<Category | null>) => setSelectedCategory(item)}
+      />
+
       {sortedDateKeys.map((key) => {
         const entries = entriesByMonth[key]
         const date = entries[0].date
@@ -65,10 +103,16 @@ export function HistoryTab() {
 
         const sum = sumOfEntries(entries, currentMonthIndex)
 
-        const maxFormatted = formatCurrency(max) //.split("$")[1]
-        const costsFormatted = formatCurrency(sum) //.split("$")[1]
+        const maxFormatted = formatCurrency(max)
+        const costsFormatted = formatCurrency(sum)
 
-        const entriesByDay = entries.reduce((map, entry) => {
+        const filteredEntries = entries.filter((entry) => {
+          return selectedCategory.value
+            ? entry.category === selectedCategory.value
+            : true
+        })
+
+        const entriesByDay = filteredEntries.reduce((map, entry) => {
           const key = getKeyForDay(entry.date)
           if (!map[key]) {
             map[key] = []
@@ -81,16 +125,14 @@ export function HistoryTab() {
           (a, b) => Number(b) - Number(a)
         )
 
+        if (filteredEntries.length === 0) {
+          return null
+        }
+
         return (
           <div key={key} className="history-group">
             <div className="history-group-title-container">
               <div className="history-group-title">{date.toFormat("LLLL")}</div>
-              {/* <div className="history-group-costs">
-                <span>{`+`}</span>
-                <span>{`${maxFormatted}`}</span>
-                <span>{`–`}</span>
-                <span>{`${costsFormatted}`}</span>
-              </div> */}
               <div className="history-group-costs-large">
                 <span>{`${costsFormatted}`}</span>
               </div>
@@ -110,51 +152,49 @@ export function HistoryTab() {
                     const cost = formatCurrency(entry.cost)
 
                     return (
-                      <>
-                        <div
-                          key={entry.id}
-                          style={{
-                            opacity:
-                              openedId !== undefined
-                                ? openedId === entry.id
-                                  ? 1
-                                  : 0.5
-                                : undefined,
-                          }}
-                          className="history-item"
-                          onClick={(event) => {
-                            event.stopPropagation()
-                            if (openedId === entry.id) {
-                              // TODO(zube): Probably should be temporary; remove behavior later.
-                              removeEntryFromCategory(entry.category, entry.id)
-                              setOpenedId(undefined)
-                            } else {
-                              setOpenedId(entry.id)
-                            }
-                          }}
-                        >
-                          <div className="history-category-outer">
-                            <div className="history-category">
-                              <span>{emojiOfCategory(entry.category)}</span>
-                              <span className="history-category-name">
-                                {stringOfCategory(entry.category)}
-                              </span>
-                            </div>
-                            <div
-                              className="history-category"
-                              style={{ marginTop: -4 }}
-                            >
-                              <span style={{ opacity: 0 }}>
-                                {emojiOfCategory(entry.category)}
-                              </span>
-                              <span className="history-category-byline">
-                                {date.toFormat("h:mm a")}
-                              </span>
-                            </div>
+                      <div
+                        key={entry.id}
+                        style={{
+                          opacity:
+                            openedId !== undefined
+                              ? openedId === entry.id
+                                ? 1
+                                : 0.5
+                              : undefined,
+                        }}
+                        className="history-item"
+                        onClick={(event) => {
+                          event.stopPropagation()
+                          if (openedId === entry.id) {
+                            // TODO(zube): Probably should be temporary; remove behavior later.
+                            removeEntryFromCategory(entry.category, entry.id)
+                            setOpenedId(undefined)
+                          } else {
+                            setOpenedId(entry.id)
+                          }
+                        }}
+                      >
+                        <div className="history-category-outer">
+                          <div className="history-category">
+                            <span>{emojiOfCategory(entry.category)}</span>
+                            <span className="history-category-name">
+                              {stringOfCategory(entry.category)}
+                            </span>
                           </div>
-                          <div className="history-cost">{cost}</div>
+                          <div
+                            className="history-category"
+                            style={{ marginTop: -4 }}
+                          >
+                            <span style={{ opacity: 0 }}>
+                              {emojiOfCategory(entry.category)}
+                            </span>
+                            <span className="history-category-byline">
+                              {date.toFormat("h:mm a")}
+                            </span>
+                          </div>
                         </div>
-                      </>
+                        <div className="history-cost">{cost}</div>
+                      </div>
                     )
                   })}
                 </div>

a28e4118bc924d35756d8b31e4b20c45e41a12f4
Nicholas Zuber <zuber.nicholas@gmail.com>
2024-06-23 16:57:24 -0400

    slower

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -43,7 +43,7 @@ export function SettingsTab({ selectedTheme, setTheme }: SettingsTabProps) {
           clearTimeout(timerRef.current)
           timerRef.current = setTimeout(() => {
             counterRef.current = 0
-          }, 500)
+          }, 1000)
         }}
       >
         {"Settings"}
